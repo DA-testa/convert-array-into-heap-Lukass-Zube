02ee178a29c9804eb5f916a2956abf6a8d9259e0
LukJr <75735232+LukJr@users.noreply.github.com>
2023-03-20 00:22:23 +0200

    commit1

--- build_heap.py
+++ build_heap.py
@@ -1,21 +1,37 @@
 # python3
 
-
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
-
+    n = len(data)
+    for i in range(n, 1, -1):
+        j = i
+        while j > 1 and data[j-1] < data[parent(j)-1]:
+            temp = data[parent(j)-1]
+            data[parent(j)-1] = data[j-1]
+            data[j-1] = temp
+            swaps.append((parent(j)-1, j-1))
+            
+            j = parent(j)
     return swaps
 
+
+def parent(child):
+    if child % 2 == 0:
+        return child // 2
+    else:
+        return (child - 1) // 2
 
 def main():
     
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
+    #text = input()
+    #if "I" == text[0]:
+    #    text = input()
+    
 
     # input from keyboard
     n = int(input())
@@ -34,9 +50,10 @@ def main():
 
     # output all swaps
     print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+    #for i, j in swaps:
+    #    print(i, j)
+    for i in swaps:
+        print(i[0], i[1])
 
 if __name__ == "__main__":
     main()
